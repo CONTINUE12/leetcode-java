550581a39c56be4d99b5a2e47743ba36eab44ab3
CONTINUE12 <468685628@qq.com>
2026-06-10 22:46:51 +0800

    func(*): 求二叉树的最大深度和最小深度

---
+++ src/main/java/com/wangli/leetcode/algorithm/P104_maxDepth.java
@@ -0,0 +1,57 @@
+package com.wangli.leetcode.algorithm;
+
+import com.wangli.leetcode.model.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class P104_maxDepth {
+
+    /**
+     * 递归，后序遍历
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        int maxDepth = Math.max(leftDepth, rightDepth);
+
+        return 1 + maxDepth;
+    }
+
+    /**
+     * 层序遍历
+     * @param root
+     * @return
+     */
+    public int maxDepthV2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int res = 0;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            res++;
+            int len = queue.size();
+            while (len > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                len--;
+            }
+        }
+
+        return res;
+    }
+}
